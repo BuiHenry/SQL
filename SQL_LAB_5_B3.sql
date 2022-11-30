4b1520bb2a28a67792cfaa8851cb793049280f33
BuiHenry <118933363+BuiHenry@users.noreply.github.com>
2022-11-30 13:26:50 +0700

    Add files via upload

---
+++ SQL_LAB_5_B3.sql
@@ -0,0 +1,82 @@
+﻿create proc sp_themphongbanmoi
+	@TENPHG nvarchar(15),
+	@MAPHG int,
+	@TRPHG nvarchar(9),
+	@NG_NHANCHUC date
+as
+begin 
+	if exists(select*from PHONGBAN where MAPHG = @MAPHG)
+	begin
+		print N'THÊM THẤT BẠI';
+		return;
+	end
+	insert into PHONGBAN(TENPHG,MAPHG,TRPHG,NG_NHANCHUC) values(@TENPHG,@MAPHG,@TRPHG,@NG_NHANCHUC);
+end;
+
+exec sp_themphongbanmoi 'CNTT',15, '005', '10-12-2020';
+
+--------------------
+
+create proc sp_capnhatphongban
+	@TENPHGCU nvarchar(15),
+	@TENPHG nvarchar(15),
+	@MAPHG int,
+	@TRPHG nvarchar(9),
+	@NG_NHANCHUC date
+as
+begin
+	update PHONGBAN
+	set TENPHG = @TENPHG,
+		MAPHG = @MAPHG,
+		TRPHG = @TRPHG,
+		NG_NHANCHUC = @NG_NHANCHUC
+	where TENPHG = @TENPHGCU;
+end;
+
+exec sp_capnhatphongban 'CNTT', 'IT', 10, '005', '1-1-2020';
+
+--------------------
+
+create proc sp_themNV
+	@HONV nvarchar(15),
+	@TENLOT nvarchar(15),
+	@TENNV nvarchar(15),
+	@MANV nvarchar(9),
+	@NGSINH datetime,
+	@DCHI nvarchar(30),
+	@PHAI nvarchar(3),
+	@LUONG float,
+	@PHG int
+as
+begin
+	if not exists(select*from PHONGBAN where TENPHG = 'IT')
+	begin
+		print N'Nhân viên phải trực thuộc phòng IT';
+		return;
+	end;
+	declare @MA_NQL nvarchar(9);
+	if @LUONG > 25000
+		set @MA_NQL = '005';
+	else
+		set @MA_NQL = '009';
+	declare @age int;
+	select @age = DATEDIFF(year,@NGSINH,getdate()) + 1;
+	if @PHAI = 'Nam' and (@age < 18 or @age >60)
+	begin
+		print N'Nam phải có độ tuổi từ 18-65';
+		return;
+	end;
+	else if @PHAI = 'Nữ' and (@age < 18 or @age >60)
+	begin
+		print N'Nữ phải có độ tuổi từ 18-60';
+		return;
+	end;
+	INSERT INTO NHANVIEN(HONV,TENLOT,TENNV,MANV,NGSINH,DCHI,PHAI,LUONG,MA_NQL,PHG)
+		VALUES(@HONV,@TENLOT,@TENNV,@MANV,@NGSINH,@DCHI,@PHAI,@LUONG,@MA_NQL,@PHG)
+end;
+
+exec sp_themNV N'Nguyễn',N'Hoàng',N'Tuấn','022','12-5-1977',N'Hà Nội','Nam',30000,6;
+5000', '004', '4')
+
+ CREATE PROC spThemNV
+ @HO
